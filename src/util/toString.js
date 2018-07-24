a15401157e11976e3acc17ec435d446a5c6593c1
wangchaoxu <wangchaoxu.fzu@qq.com>
2018-07-24 17:19:43 +0800

    修复安卓正式版打包图表中的中文不能正常显示的BUG

--- src/util/toString.js
+++ src/util/toString.js
@@ -1,13 +1,15 @@
 export default function toString(obj) {
-  let result = JSON.stringify(obj, function(key, val) {
+    let result = JSON.stringify(obj, function(key, val) {
         if (typeof val === 'function') {
             return `~--demo--~${val}~--demo--~`;
         }
         return val;
     });
-
     do {
-        result = result.replace('\"~--demo--~', '').replace('~--demo--~\"', '').replace(/\\n/g, '').replace(/\\\"/g,"\"");//最后一个replace将release模式中莫名生成的\"转换成"
-    } while (result.indexOf('~--demo--~') >= 0);
+        //最后一个replace将release模式中莫名生成的 \" 转换成 "
+        result = result.replace('\"~--demo--~', '').replace('~--demo--~\"', '').replace(/\\n/g, '').replace(/\\\"/g,"\"");
+    } while (result.indexOf('~--demo--~') > -1);
+    // 添加此行把unicode转为中文，目前最省心的方法，其他方法稍麻烦
+    result = unescape(result.replace(/\\u/g, "%u"));
     return result;
 }
